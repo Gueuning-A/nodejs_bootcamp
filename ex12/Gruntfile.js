734f3367a22ce7df3f896de90bf674b85496f3db
8108_web (Student) <s_8108_web@PEDAGOGIQUE.LAN>
2015-12-04 16:30:26 +0100

    last version

--- ex12/Gruntfile.js
+++ ex12/Gruntfile.js
@@ -15,6 +15,19 @@ module.exports = function(grunt) {
           }
         }
       },
+      handlebarslayouts:{
+        dist:{
+          files:{
+            'dist/*.html':'src/hbs/pages/*.hbs'
+          },
+          options: {
+            partials: [
+              'src/hbs/partials/*.hbs'
+            ],
+            context: 'src/hbs/datas.json'
+          }
+        }
+      },
       copy: {
         dist: {
           files: [{
@@ -23,7 +36,25 @@ module.exports = function(grunt) {
             src: ['*.html'],
             dest: '<%= files.destination %>'
           }]
-        },
+        }
+      },
+      compass: {
+        dist: {
+          options: {
+            sassDir: 'src/sass',
+            cssDir: 'dist/css',
+            environment: 'production'
+          }
+        }},
+      imagemin: {
+        dist: {                         // Another target
+          files: [{
+            expand: true,                  // Enable dynamic expansion
+            cwd: 'src/',                   // Src matches are relative to this path
+            src: ['img/**/*.{png,jpg,gif}'],   // Actual patterns to match
+            dest: '<%= files.destination %>'                  // Destination path prefix
+          }]
+        }
       },
       connect: {
         server: {
@@ -40,11 +71,15 @@ module.exports = function(grunt) {
         },
         js: {
           files: '<%= files.js %>',
-          tasks: ['uglify'],
+          tasks: ['uglify:dist'],
         },
-        html: {
-            files: 'src/*.html',
-            tasks: ['copy:dist'],
+        sass:{
+          files: 'src/sass/**/*.scss',
+          tasks:['compass:dist']
+        },
+        hbs: {
+            files: ['src/hbs/**/**/*.hbs','src/hbs/**/**/*.json'],
+            tasks: ['handlebarslayouts'],
           },
       },
 
@@ -52,12 +87,16 @@ module.exports = function(grunt) {
   });
 
 // Load the plugin that provides the "uglify" task.
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify'); //concatène les fichiers js
+grunt.loadNpmTasks('grunt-contrib-copy'); // copie les fichiers
+grunt.loadNpmTasks('grunt-contrib-connect'); //pour lancer le serveur
+grunt.loadNpmTasks('grunt-contrib-watch'); //relance le grunt a chaque modif
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-contrib-compass');
+grunt.loadNpmTasks('grunt-handlebars-layouts');
 
 // Default task(s).
-grunt.registerTask('default', ['uglify:dist', 'copy:dist','connect','watch']);
+grunt.registerTask('default', ['uglify:dist', 'handlebarslayouts','compass:dist','connect','watch']);
+grunt.registerTask('push2prod', ['clean', 'uglify:dist','handlebarslayouts','compass','ftpush:prod']);
 
 };
